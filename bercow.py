8181d63f0c67400fc2360a6352f651128ea214b0
chrisalexander <chris@chris-alexander.co.uk>
2020-03-16 20:28:48 +0000

    python tidy-up

--- bercow.py
+++ bercow.py
@@ -1,25 +1,33 @@
-if __name__ == '__main__':
-    import systemd.daemon
-    import os
-    import redis
+import systemd.daemon
+import os
+import redis
 
+
+def execute():
     print('ORDER')
-    os.system("amixer set PCM unmute")
-    os.system("amixer set PCM 100%")
+
+    os.system('amixer set PCM unmute')
+    os.system('amixer set PCM 100%')
+
     r = redis.Redis(host='192.168.0.1', port=6379,
                     db=0, decode_responses=True)
     p = r.pubsub(ignore_subscribe_messages=True)
     p.subscribe('bercow')
+
     print('ORDER')
     systemd.daemon.notify('READY=1')
 
     try:
         for message in p.listen():
-            cmd = message["data"]
-            filepath = "/home/pi/zoidberg-deploy/bercow/Audio/" + cmd + ".WAV"
+            cmd = message['data']
+            filepath = '/home/pi/zoidberg-deploy/bercow/Audio/' + cmd + '.WAV'
             if os.path.isfile(filepath):
-                os.system("aplay " + filepath + " &")
+                os.system('aplay ' + filepath + ' &')
 
     except:
         p.close()
-        print("ORDER")
+        print('ORDER')
+
+
+if __name__ == '__main__':
+    execute()
